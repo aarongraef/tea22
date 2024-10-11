f87d37e5cb31ff2ee4ad3bb10f6016359e307096
aarongraef <126180445+aarongraef@users.noreply.github.com>
2024-10-11 16:20:24 +0200

    kommentare in main

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -1,20 +1,20 @@
-#include <fmt/chrono.h>
-#include <fmt/format.h>
+#include <fmt/chrono.h>         //fmt bib
+#include <fmt/format.h>         
 
 #include "CLI/CLI.hpp"
 #include "config.h"
 
-auto main(int argc, char **argv) -> int
+auto main(int argc, char **argv) -> int         //funktion (leere parameterliste) ->Rückgabewert
 {
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
-    CLI::App app{PROJECT_NAME};
+    CLI::App app{PROJECT_NAME};         // initialisiere cli objekt, dem ich einen Projektnamen geben
     try
     {
-        app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
-        app.parse(argc, argv);
+        app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));        // hier projekt version und build datum einfügen
+        app.parse(argc, argv);          // returncode, bei false gibts Fehlerbehandlung
     }
     catch (const CLI::ParseError &e)
     {
@@ -26,9 +26,9 @@ auto main(int argc, char **argv) -> int
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
+    fmt::print("Hello, {}!\n", app.get_name());         //python ähnlich
 
     /* INSERT YOUR CODE HERE */
 
-    return 0; /* exit gracefully*/
+    return 0; /* exit gracefully*/          //return code
 }
